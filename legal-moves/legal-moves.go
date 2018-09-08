f51b619f6837a252a191af554fa6e806b61cf8f1
Tony Oreglia <tony.oreglia@safetyculture.io>
2018-09-08 14:00:07 +1000

    Printed hashtables to file

--- legal-moves/legal-moves.go
+++ legal-moves/legal-moves.go
@@ -1,12 +1,21 @@
+// Package legalmoves calculates all legal chess moves
+// from a single given chess position, which is represented
+// using the position package.
 package legalmoves
+
+import (
+	"github.com/tonyoreglia/glee/position"
+)
 
 type LegalMoves struct {
 	// slice with capacity of 100 moves, starting len of 0
 	moves [][2]int
+	pos   position.Position
 }
 
-func NewLegalMoves() *LegalMoves {
-	legalMoves := &LegalMoves{}
-	legalMoves.moves = make([][2]int, 0, 100)
-	return legalMoves
+func NewLegalMoves(pos position.Position) *LegalMoves {
+	moves := &LegalMoves{}
+	moves.moves = make([][2]int, 0, 100)
+	moves.pos = pos
+	return moves
 }
