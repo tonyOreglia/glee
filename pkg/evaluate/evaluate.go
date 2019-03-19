b1eead0b26544bc53575805e0e0f018172ee34de
Tony Oreglia <tony.oreglia@safetyculture.io>
2019-03-19 22:02:01 +1100

    implemnent time machine

--- pkg/evaluate/evaluate.go
+++ pkg/evaluate/evaluate.go
@@ -27,5 +27,49 @@ func EvaluatePosition(pos *position.Position) int {
 	score += 100 * (whitePawnsBb.PopulationCount() - blackPawnsBb.PopulationCount())
 	score += 890 * (whiteQueenBb.PopulationCount() - blackQueensBb.PopulationCount())
 
+	score += kingBonusWhite[whiteKingBb.Msb()]
+	score -= kingBonusBlack[blackKingBb.Msb()]
+
+	if whiteBishopsBb.PopulationCount() > 1 {
+		score += 15
+	}
+
+	if blackBishopsBb.PopulationCount() > 1 {
+		score -= 15
+	}
+
+	for !whiteBishopsBb.IsZero() {
+		msb := whiteBishopsBb.Msb()
+		score += bishopBonusWhite[msb]
+		whiteBishopsBb.RemoveBit(msb)
+	}
+	for !blackBishopsBb.IsZero() {
+		msb := blackBishopsBb.Msb()
+		score -= bishopBonusBlack[msb]
+		blackBishopsBb.RemoveBit(msb)
+	}
+
+	for !whiteKnightsBb.IsZero() {
+		msb := whiteKnightsBb.Msb()
+		score += knightBonus[msb]
+		whiteKnightsBb.RemoveBit(msb)
+	}
+	for !blackKnightsBb.IsZero() {
+		msb := blackKnightsBb.Msb()
+		score -= knightBonus[msb]
+		blackKnightsBb.RemoveBit(msb)
+	}
+
+	for !whitePawnsBb.IsZero() {
+		msb := whitePawnsBb.Msb()
+		score += pawnBonusWhite[msb]
+		whitePawnsBb.RemoveBit(msb)
+	}
+	for !blackPawnsBb.IsZero() {
+		msb := blackPawnsBb.Msb()
+		score -= pawnBonusBlack[msb]
+		blackPawnsBb.RemoveBit(msb)
+	}
+
 	return score
 }
--- pkg/evaluate/evaluate_test.go
+++ pkg/evaluate/evaluate_test.go
@@ -14,9 +14,9 @@ func TestEvaluatePosition(t *testing.T) {
 
 	pos, _ = position.NewPositionFen("k7/8/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
 	score = EvaluatePosition(pos)
-	assert.Equal(t, 4010, score)
+	assert.True(t, score > 3000)
 
 	pos, _ = position.NewPositionFen("rnbqkbnr/pppppppp/8/8/8/8/8/7K w KQkq - 0 1")
 	score = EvaluatePosition(pos)
-	assert.Equal(t, -4010, score)
+	assert.True(t, score < -3000)
 }
